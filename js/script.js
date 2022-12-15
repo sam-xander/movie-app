2b5093636f1041fe241b5d10d1802e53e2629793
birda <birdaNFT@gmail.com>
2022-12-15 20:22:15 +0000

    completed jquery version

--- js/script.js
+++ js/script.js
@@ -1,55 +1,45 @@
-var searchInput = document.querySelector(".search");
-var cardWrapper = document.querySelector("main");
+var searchInput = $(".search");
+var cardWrapper = $("main");
 
 function noMatch() {
-  cardWrapper.innerHTML = `<p class="no-search">No results found.</p>`;
+  cardWrapper.html(`<p class="no-search">No results found.</p>`);
 }
 
 function displayMatches(matches) {
-  cardWrapper.innerHTML = "";
+  cardWrapper.html("");
 
   if (!matches) {
     noMatch();
   } else {
     for (var matchObj of matches) {
-      cardWrapper.insertAdjacentHTML(
-        "beforeend",
-        `
+      cardWrapper.append(`
         <div class="movie-card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
         url(${matchObj.Poster})">
           <h3>${matchObj.Title}</h3>
           <p>${matchObj.Year}</p>
           <a href="https://www.imdb.com/title/${matchObj.imdbID}/" target="_blank">More Info</a>
         </div>
-        `
-      );
+        `);
     }
   }
 }
 
 function fetchMovies(event) {
   var keyCode = event.keyCode;
-  var searchText = searchInput.value.toLowerCase().trim();
+  var searchText = searchInput.val().trim();
 
   if (keyCode === 13 && searchText) {
-    var responsePromise = fetch(
-      `https://www.omdbapi.com/?apikey=96756e0a&s=${searchText}`
+    $.get(`https://www.omdbapi.com/?apikey=96756e0a&s=${searchText}`).then(
+      function (data) {
+        displayMatches(data.Search);
+        searchInput.val("");
+      }
     );
-
-    function handleResponse(responseObj) {
-      return responseObj.json();
-    }
-
-    responsePromise.then(handleResponse).then(function (data) {
-      displayMatches(data.Search);
-
-      searchInput.value = "";
-    });
   }
 }
 
 function init() {
-  searchInput.addEventListener("keydown", fetchMovies);
+  searchInput.keydown(fetchMovies);
 }
 
 init();
